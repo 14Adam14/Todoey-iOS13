b59307b72037e689abeaafc354177385f631d72d
Adham <adam.bolotov@gmail.com>
2022-02-13 05:03:21 +0100

    start

---
+++ Todoey/TodoViewController.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
